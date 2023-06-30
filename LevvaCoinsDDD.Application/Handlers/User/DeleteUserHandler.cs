f8ad1887344769fba05ada01eb622f9f528a13a0
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-06-30 10:05:44 -0300

    feat: add DeleteUser useCase

---
+++ LevvaCoinsDDD.Application/Handlers/User/DeleteUserHandler.cs
@@ -0,0 +1,28 @@
+﻿using LevvaCoinsDDD.Application.Commands.Requests.User;
+using LevvaCoinsDDD.Application.Dtos;
+using LevvaCoinsDDD.Infra.Data.Interfaces.Repositories;
+using MediatR;
+
+namespace LevvaCoinsDDD.Application.Handlers.User;
+public class DeleteUserHandler : IRequestHandler<DeleteUserRequest, ResponseApiDTO<bool>>
+{
+    private readonly IUserRepository _userRepository;
+
+    public DeleteUserHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<ResponseApiDTO<bool>> Handle(DeleteUserRequest request, CancellationToken cancellationToken)
+    {
+        var guidId = Guid.Parse(request.Id);
+
+        var user = await _userRepository.GetByIdAsync(guidId);
+
+        if (user == null) return new ResponseApiDTO<bool> { hasError = true, message = "Esse usuário não existe." };
+
+        await _userRepository.DeleteAsync(guidId);
+
+        return new ResponseApiDTO<bool> { hasError = false };
+    }
+}
