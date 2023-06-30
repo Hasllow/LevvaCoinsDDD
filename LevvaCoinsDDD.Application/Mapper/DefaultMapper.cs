c56642cb7c32fe74914605d3bfc4db8fbfb55d09
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-06-30 10:08:32 -0300

    feat: update mapper

--- LevvaCoinsDDD.Application/Mapper/DefaultMapper.cs
+++ LevvaCoinsDDD.Application/Mapper/DefaultMapper.cs
@@ -4,6 +4,7 @@ using LevvaCoinsDDD.Application.Commands.Response.User;
 using LevvaCoinsDDD.Application.Dtos.Category;
 using LevvaCoinsDDD.Application.Dtos.Transaction;
 using LevvaCoinsDDD.Application.Dtos.User;
+using LevvaCoinsDDD.Application.Queries.Responses.User;
 using LevvaCoinsDDD.Domain.Models;
 
 namespace LevvaCoinsDDD.Application.Mapper;
@@ -15,8 +16,13 @@ public class DefaultMapper : Profile
         CreateMap<User, UserUpdateDTO>().ReverseMap();
         CreateMap<User, LoginValuesDTO>().ReverseMap();
 
+        CreateMap<User, LoginUserResponse>().ReverseMap();
         CreateMap<User, CreateUserRequest>().ReverseMap();
         CreateMap<User, CreateUserResponse>().ReverseMap();
+        CreateMap<User, GetUserByIdResponse>().ReverseMap();
+        CreateMap<User, GetAllUsersResponse>().ReverseMap();
+
+
 
         CreateMap<Transaction, TransactionDTO>().ReverseMap();
         CreateMap<Transaction, TransactionNewDTO>().ReverseMap();
