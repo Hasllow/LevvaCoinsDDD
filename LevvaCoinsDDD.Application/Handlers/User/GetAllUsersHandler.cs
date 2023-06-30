9e41358d408eaeb15ba4723a6e341a8898b9ecda
Gustavo B. Cassalho <gustavo.cassalho@levva.io>
2023-06-30 10:00:32 -0300

    feat: add GetAllUsers useCase

---
+++ LevvaCoinsDDD.Application/Handlers/User/GetAllUsersHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using LevvaCoinsDDD.Application.Dtos;
+using LevvaCoinsDDD.Application.Queries.Requests.User;
+using LevvaCoinsDDD.Application.Queries.Responses.User;
+using LevvaCoinsDDD.Infra.Data.Interfaces.Repositories;
+using MediatR;
+
+namespace LevvaCoinsDDD.Application.Handlers.User;
+public class GetAllUsersHandler : IRequestHandler<GetAllUsersRequest, ResponseApiDTO<GetAllUsersResponse>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllUsersHandler(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<ResponseApiDTO<GetAllUsersResponse>> Handle(GetAllUsersRequest request, CancellationToken cancellationToken)
+    {
+        var response = await _userRepository.GetAllAsync();
+        var mappedUsers = _mapper.Map<IEnumerable<GetAllUsersResponse>>(response);
+
+        return new ResponseApiDTO<GetAllUsersResponse> { hasError = false, collectionData = mappedUsers };
+    }
+}
---
+++ LevvaCoinsDDD.Application/Queries/Responses/User/GetAllUsersResponse.cs
@@ -0,0 +1,6 @@
+﻿using LevvaCoinsDDD.Application.Dtos.User;
+
+namespace LevvaCoinsDDD.Application.Queries.Responses.User;
+public class GetAllUsersResponse : UserDTO
+{
+}
